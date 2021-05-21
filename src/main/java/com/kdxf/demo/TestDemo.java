272b9afd12058df64bf1e05fc9561a20c8170651
zoudmbean <zoudmbean@163.com>
2021-05-21 12:55:59 +0800

    idea第一次提交

---
+++ src/main/java/com/kdxf/demo/TestDemo.java
@@ -0,0 +1,10 @@
+package com.kdxf.demo;
+
+/**
+ * @createDate:2021/5/21
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
