3ec0ef75f44bac52c5d3ddc7143f4f3ff3b7ec96
zoudmbean <zoudmbean@163.com>
2021-05-21 12:58:55 +0800

    idea第二次提交

--- src/main/java/com/kdxf/demo/TestDemo.java
+++ src/main/java/com/kdxf/demo/TestDemo.java
@@ -7,5 +7,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
